a2c1ea60a213cdf4a283582ba42e4112babcd7f3
taha <taha.turedi@powercoders.org>
2024-04-26 14:02:36 +0200

    Upload New Exercise

---
+++ Week_3/Number_of_characters/README.txt
@@ -0,0 +1,3 @@
+Exercise Counting the number of characters
+
+Create a program in JavaScript that prompts for an input string and displays output that shows the input string and the number of characters the string contains.
---
+++ Week_3/Number_of_characters/script.js
@@ -0,0 +1,12 @@
+
+let inputString = prompt("Please enter a string: ");
+
+
+let numberOfCharacters = inputString.length;
+
+
+let outputMessage = "Input String: " + inputString + "\n";
+outputMessage += "Number of Characters: " + numberOfCharacters;
+
+
+alert(outputMessage);
