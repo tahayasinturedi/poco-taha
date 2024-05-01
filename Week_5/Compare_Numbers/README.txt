970ca761e930e024c2917126c342d45cb31a9db4
taha <taha.turedi@powercoders.org>
2024-05-01 13:47:28 +0200

    Upload Compare Numbers Exercise

---
+++ Week_5/Compare_Numbers/README.txt
@@ -0,0 +1,6 @@
+Which number is bigger?
+
+Declare 2 variables, both numbers
+Compare which number is greater
+Log the output, e.g. "The greater number of 5 and 10 is 10."
+Add an output for the else statement, e.g. "The smaller number of 5 and 10 is 5."
---
+++ Week_5/Compare_Numbers/script.js
@@ -0,0 +1,10 @@
+let number1 = 3;
+let number2 = 9;
+
+if(number1<number2){
+    console.log("The smaller number of " +number1+ " and " + number2 + " is " +number1)
+} else if (number1 === number2 ){
+    console.log("The numbers are equal")
+} else {
+    console.log("The smaller number of " +number1+ " and " + number2 + " is " +number2)
+}
