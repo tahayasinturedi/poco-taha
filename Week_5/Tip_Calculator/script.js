bfc84ad5c8c9a85abe7d9716159389ae2af89548
taha <taha.turedi@powercoders.org>
2024-05-01 15:14:45 +0200

    Update Tip Calculator

--- Week_5/Tip_Calculator/script.js
+++ Week_5/Tip_Calculator/script.js
@@ -1,12 +1,12 @@
 let totalAmount = +window.prompt("Please enter your total amount: ");
-    console.log("Your total amount is " + totalAmount+" Frank")
+    console.log("Your total amount is " + totalAmount+" CHF")
 let tipPercentage = +window.prompt("Please enter your desired tip percentage: ");
-    console.log("Your desired tip percentage is " + tipPercentage + " Frank");
+    console.log("Your desired tip percentage is " + tipPercentage + " Percentage");
 
 let tipAmount = (totalAmount*tipPercentage)/100;
 
-    console.log("Your tip is " +tipAmount+ " Frank");
+    console.log("Your tip is " +tipAmount+ " CHF");
 
 let overallAmount = tipAmount + totalAmount ;   
 
-    console.log("Your final payment is " +overallAmount+ " Frank" )
+    console.log("Your final payment is " +overallAmount+ " CHF" );
